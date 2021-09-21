552801669babf7eedfe319050c0b4a814a202eb6
sofyadetkovskaya <sofyadetkovskaya@gmail.com>
2021-09-22 02:47:01 +0300

    stealed af

---
+++ affrMid.js
@@ -0,0 +1,37 @@
+function generateM() {
+    var afrmt = [ 
+   
+    '<img src="images/imgMid/middle8.png">',
+    '<img src="images/imgMid/middle9_Монтажная область 1.png">',
+    '<img src="images/imgMid/middle10_Монтажная область 1.png">',
+    '<img src="images/imgMid/middle11_Монтажная область 1.png">',
+    '<img src="images/imgMid/middle12_Монтажная область 1.png">',
+    '<img src="images/imgMid/middle13_Монтажная область 1.png">',
+    '<img src="images/imgMid/middle14_Монтажная область 1.png">',
+    '<img src="images/imgMid/middle15_Монтажная область 1.png">',
+    '<img src="images/imgMid/middle16_Монтажная область 1.png">',
+    '<img src="images/imgMid/middle17_Монтажная область 1.png">',
+    '<img src="images/imgMid/middle18_Монтажная область 1.png">',
+    '<img src="images/imgMid/middle19_Монтажная область 1.png">',
+    '<img src="images/imgMid/middle20_Монтажная область 1.png">',
+    '<img src="images/imgMid/middle21_Монтажная область 1.png">',
+    '<img src="images/imgMid/middle22_Монтажная область 1.png">',
+    '<img src="images/imgMid/middle23_Монтажная область 1.png">',
+    '<img src="images/imgMid/middle24_Монтажная область 1.png">',
+    '<img src="images/imgMid/middle25_Монтажная область 1.png">',
+    '<img src="images/imgMid/middle26_Монтажная область 1.png">',
+    '<img src="images/imgMid/middle27_Монтажная область 1.png">',
+    '<img src="images/imgMid/middle28_Монтажная область 1.png">',
+    '<img src="images/imgMid/middle29_Монтажная область 1.png">',
+    '<img src="images/imgMid/middle30_Монтажная область 1.png">',
+    '<img src="images/imgMid/middle31_Монтажная область 1.png">',
+    '<img src="images/imgMid/middle32_Монтажная область 1.png">',
+    '<img src="images/imgMid/middle33_Монтажная область 1.png">',
+    '<img src="images/imgMid/middle34_Монтажная область 1.png">',
+    '<img src="images/imgMid/middle35_Монтажная область 1.png">',
+    
+];
+
+var m=document.querySelector(".div2");
+m.innerHTML=afrmt[Math.floor(Math.random()*afrmt.length)];
+}
